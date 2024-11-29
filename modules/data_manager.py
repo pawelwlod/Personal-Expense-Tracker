5b913be3fc27151677e8c373b14e6c4f92f4314b
Darth-Fortis <pawel.wlodarski09@gmail.com>
2024-11-29 23:27:34 +0000

    0.0.4 - Load and Display Expenses

--- modules/data_manager.py
+++ modules/data_manager.py
@@ -5,11 +5,18 @@ import json
 
 class DataManager:
     '''Manages expense data for Personal Expense Tracker.'''
+    def __init__(self):
+        self.file_name = 'data/expenses.json'
     
     def save_data(self, data):
         '''Save data to file'''
-        file_name = 'data/expenses.json'
-        
-        with open(file_name, 'a') as file:
+        with open(self.file_name, 'w') as file:
             json.dump(data, file, indent=4, separators=(',', ': '))
-            print(f"\nData saved to: {file_name} ")
+    
+    def load_data(self, default):
+        '''Load data from file'''
+        try:
+            with open(self.file_name, 'r') as file:
+                return json.load(file)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            return default
