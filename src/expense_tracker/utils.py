7272067f26a8a523f64905768f6d7e141d59c0dc
pawel <pawelwlod@proton.me>
2026-09-08 20:25:01 +0100

    Update budget_checks to use budget_message

--- src/expense_tracker/utils.py
+++ src/expense_tracker/utils.py
@@ -11,18 +11,18 @@ class Utils:
         '''Checks budgets and returns any relevant alerts/warning.'''
         try:
             self.bm.spent_period(expenses)
-            check = False
+            check    = False
+            messages = []
             for name, budget in self.bm.budgets.items():
-                if budget["options"] == "alert":
-                    print(f"\nALERT! You have passed your {budget["period"]} {name} budget: Spent - £{budget["spent"]}, Remaining - £{budget["remaining"]}")
+                if budget["options"] in ["alert", "warn"]:
                     check = True
-                elif budget["options"] == "warn":
-                    print(f"\nWARNING! You are close to your {budget["period"]} {name} budget: Spent - £{budget["spent"]}, Remaining - £{budget["remaining"]}")
-                    check = True
+                    messages.append(self.budget_message(name, budget))
             if not check:
-                print("\nNo Budget Warnings/Alerts!")
+                messages.append("No Budget Warnings/Alerts!")
+
+            return messages
         except Exception as e:
-            print(f"Error in budget_checks: {str(e)}")
+            return f"Error in budget_checks: {str(e)}"
 
     def budget_message(self, name, budget: dict):
         '''Creates message for each relevant budget alert/warning.'''
